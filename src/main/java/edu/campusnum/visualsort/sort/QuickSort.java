820cb80e7e898bdad2f24768a0a706c562abcac8
Jocelyn Grelier <grelierjocelyn@gmail.com>
2024-10-17 14:11:12 +0200

    tri rapide

--- src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
@@ -10,7 +10,33 @@ import edu.campusnum.visualsort.model.ObservableArray;
  */
 public class QuickSort implements SortAlgorithm{
     @Override
+
     public void sort(ObservableArray array) {
-        throw new UnsupportedOperationException("Not implemented");
+
+        tri_rapide(array, 0, array.getLength() - 1);
+    }
+
+    public void tri_rapide(ObservableArray array, int first , int last) {
+
+        if (first < last) {
+            int pi = partitionener(array,first , last);
+            tri_rapide( array,first, pi -1);
+            tri_rapide(array, pi + 1, last);
+
+        }
+    }
+
+    public int partitionener(ObservableArray array, int first , int last) {
+        int pivot = last;
+        int j = first;
+
+        for (int i = first; i <= pivot -1; i++) {
+            if (array.get(i) <= array.get(pivot)) {
+                array.swap(i , j);
+                j = j + 1;
+            }
+        }
+            array.swap(last, j);
+            return j;
     }
 }
